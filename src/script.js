dc3dd68086b6d3e4a892347cb5ad4bafb602b629
jess-uu <81002420+jess-uu@users.noreply.github.com>
2023-05-09 17:17:47 +1000

    Week7 Finessing
    Updated API source and related code, CSS and HTML updates to design of the page

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 let celsiusActive = true;
 let cityResult = "Melbourne";
-let apiKey = "b2613fc2433dc611b2f664d7a243d6bd";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
-
+//let apiKey = "b2613fc2433dc611b2f664d7a243d6bd";
+let apiKey = "dc051b10a64333cf3ob6tb0e6afc3da3";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=`;
 // update city name
 function getCity(event) {
   event.preventDefault();
@@ -10,11 +11,10 @@ function getCity(event) {
   if (city.value) {
     let cityResult = `${city.value}`;
     axios
-      .get(`${apiUrl}${cityResult}&appid=${apiKey}&units=metric`)
+      .get(`${apiUrl}${cityResult}&key=${apiKey}&units=metric`)
       .then(showTemperature);
     celsiusActive = true;
   } else {
-    alert("Please search for a city");
   }
 }
 
@@ -29,6 +29,8 @@ function convertFH(event) {
     let temp = parseInt(updateFH.innerHTML);
     updateFH.innerHTML = `${Math.round((temp * 9) / 5 + 32)}°F`;
     celsiusActive = false;
+    convertCelsius.classList.remove("active");
+    convertFahrenheit.classList.add("active");
   } else {
   }
 }
@@ -42,6 +44,8 @@ function convertCS(event) {
     let convertCS = Math.round(((temp - 32) * 5) / 9);
     updateCS.innerHTML = `${convertCS}°C`;
     celsiusActive = true;
+    convertFahrenheit.classList.remove("active");
+    convertCelsius.classList.add("active");
   } else {
   }
 }
@@ -58,6 +62,23 @@ function updateDateTime() {
     "Saturday",
   ];
   let day = days[currentDate.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let date = currentDate.getDate();
+  let month = months[currentDate.getMonth()];
+  let year = currentDate.getFullYear();
   let hour = currentDate.getHours();
   if (hour < 10) {
     hour = `0${hour}`;
@@ -66,11 +87,10 @@ function updateDateTime() {
   if (minute < 10) {
     minute = `0${minute}`;
   }
-  return `${day}. ${hour}:${minute}`;
+  return `${day} ${date} ${month} ${year}, ${hour}:${minute}`;
 }
 let date = document.querySelector("h2#datetime");
 let currentDate = new Date();
-
 date.innerHTML = updateDateTime(currentDate);
 
 // capture temp conversion clicks
@@ -82,30 +102,103 @@ convertCelsius.addEventListener("click", convertCS);
 
 // show temperature of searched city
 function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
+  let temperature = Math.round(response.data.temperature.current);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = `${temperature}°C`;
   let temperatureDesc = document.querySelector("#temperature-description");
-  temperatureDesc.innerHTML = response.data.weather[0].description;
+  temperatureDesc.innerHTML = response.data.condition.description;
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  humidity.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = `Windspeed: ${Math.round(
     response.data.wind.speed
   )}km/h`;
   let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.name;
+  h1.innerHTML = response.data.city;
+  let updateTime = document.querySelector(".last-updated");
+  updateTime.innerHTML = formatDate(response.data.time * 1000);
+  let currentIcon = document.querySelector("#main-icon");
+  currentIcon.setAttribute("src", `${response.data.condition.icon_url}`);
+  currentIcon.setAttribute("alt", `${response.data.condition.description}`);
+}
+// show temperature of geolocation city
+function showTemperatureLoc(response) {
+  let temperature = Math.round(response.data.daily[0].temperature.day);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}°C`;
+  let temperatureDesc = document.querySelector("#temperature-description");
+  temperatureDesc.innerHTML = response.data.daily[0].condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.daily[0].temperature.humidity}%`;
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML = `Windspeed: ${Math.round(
+    response.data.daily[0].wind.speed
+  )}km/h`;
+  let currentIcon = document.querySelector("#main-icon");
+  currentIcon.setAttribute(
+    "src",
+    `${response.data.daily[0].condition.icon_url}`
+  );
+  currentIcon.setAttribute(
+    "alt",
+    `${response.data.daily[0].condition.description}`
+  );
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+  let updateTime = document.querySelector(".last-updated");
+  updateTime.innerHTML = formatDate(response.data.daily[0].time * 1000);
 }
 axios
-  .get(`${apiUrl}${cityResult}&appid=${apiKey}&units=metric`)
+  .get(`${apiUrl}${cityResult}&key=${apiKey}&units=metric`)
   .then(showTemperature);
+
+// API weather last updated
+function formatDate(timestamp) {
+  let updateDate = new Date(timestamp);
+  let hours = updateDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = updateDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[updateDate.getDay()];
+  let date = updateDate.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[updateDate.getMonth()];
+  let year = updateDate.getFullYear();
+  return `Updated: ${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
+}
 
 // show temperature of current location
 function showPosition(position) {
   let long = position.coords.longitude;
   let lat = position.coords.latitude;
-  let locUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  axios.get(`${locUrl}`).then(showTemperature);
+  let locUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${long}&lat=${lat}&key=${apiKey}`;
+  axios.get(`${locUrl}`).then(showTemperatureLoc);
   celsiusActive = true;
 }
 
